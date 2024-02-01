a96278efb1e252492d4aa18eaa852a715be6462b
Henryk Krzyszczak <henryk.krzyszczak@intive.com>
2024-02-01 07:50:23 +0100

    deriving values, autputting questions and registering answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,33 @@
 import { useState } from 'react';
+import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestionIndex] = useState(0);
-  const [userAnswers, setUserAnswer] = useState([]);
-  return <p>Currently active Question</p>;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li id={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
